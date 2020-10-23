f3b088700ea51bd50fd5deb8b2f883f3575c2b9e
Jesse Glick <jglick@cloudbees.com>
2020-10-23 19:33:37 -0400

    Use -Pquick-build

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -2,5 +2,5 @@
 set -euxo pipefail
 # see https://github.com/jenkinsci/incrementals-tools/#superseding-maven-releases
 export MAVEN_OPTS=-Djansi.force=true
-mvn -B -V -s .github/workflows/settings.xml -ntp -Dstyle.color=always -Dset.changelist -DaltDeploymentRepository=maven.jenkins-ci.org::default::https://repo.jenkins-ci.org/releases/ -DskipTests clean deploy
+mvn -B -V -s .github/workflows/settings.xml -ntp -Dstyle.color=always -Dset.changelist -DaltDeploymentRepository=maven.jenkins-ci.org::default::https://repo.jenkins-ci.org/releases/ -Pquick-build clean deploy
 # TODO https://help.github.com/en/articles/virtual-environments-for-github-actions#github_token-secret use GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }} to define a release based on this version, after configuring Release Drafter as a step

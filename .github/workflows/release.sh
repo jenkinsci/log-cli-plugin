edc9cd18cbdc283f5120dab8f0523305e7a88e91
Jesse Glick <jglick@cloudbees.com>
2020-10-30 18:14:40 -0400

    Release Drafter silently did nothing; maybe we need to create a tag?

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -3,4 +3,7 @@ set -euxo pipefail
 # see https://github.com/jenkinsci/incrementals-tools/#superseding-maven-releases
 export MAVEN_OPTS=-Djansi.force=true
 mvn -B -V -s .github/workflows/settings.xml -ntp -Dstyle.color=always -Dset.changelist -DaltDeploymentRepository=maven.jenkins-ci.org::default::https://repo.jenkins-ci.org/releases/ -Pquick-build clean deploy
-echo '::set-output name=version::'$(mvn -B -ntp -Dset.changelist -Dexpression=project.version -q -DforceStdout help:evaluate)
+version=$(mvn -B -ntp -Dset.changelist -Dexpression=project.version -q -DforceStdout help:evaluate)
+echo "::set-output name=version::$version"
+# TODO would be more legible with jo:
+curl -H "Authorization: Bearer $GITHUB_TOKEN" -s -d '{"refs":"refs/tags/'$version'","sha":"'$GITHUB_SHA'"}' $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/git/refs

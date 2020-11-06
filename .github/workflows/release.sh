b576514f584026d21d1e36f6a55669dab9e83b5a
Jesse Glick <jglick@cloudbees.com>
2020-11-06 16:43:31 -0500

    Rearranging how Release Drafter is run

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -4,6 +4,7 @@ set -euxo pipefail
 export MAVEN_OPTS=-Djansi.force=true
 mvn -B -V -s .github/workflows/settings.xml -ntp -Dstyle.color=always -Dset.changelist -DaltDeploymentRepository=maven.jenkins-ci.org::default::https://repo.jenkins-ci.org/releases/ -Pquick-build clean deploy
 version=$(mvn -B -ntp -Dset.changelist -Dexpression=project.version -q -DforceStdout help:evaluate)
-echo "::set-output name=version::$version"
 # TODO would be more legible with jo:
 curl -H "Authorization: Bearer $GITHUB_TOKEN" -s -d '{"ref":"refs/tags/'$version'","sha":"'$GITHUB_SHA'"}' $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/git/refs
+release=$(curl -H "Authorization: Bearer $GITHUB_TOKEN" -s $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/releases | jq -e -r '.[] | select(.draft == true and .name == "next") | .id')
+curl -H "Authorization: Bearer $GITHUB_TOKEN" -X PATCH -d '{"draft":"false","name":"'$version'","tag_name":"'$version'"}' $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/releases/$release
